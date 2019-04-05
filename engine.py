7ce4852c2bfe1823869c918730b57255033e22f2
LegoBricker <dylan@mygreenfamily.us>
2019-04-05 12:34:26 -0700

    Add first implementation of the image classifier powered brain.
    - Current network structure has two skip layers and achieves 49% accuracy when
    tasked with identifying black/white/draws.
    - The new AI can convincingly and handily beat the random ai, but has difficulty
    recognizing forced checkmate sequences and thus tends to stalemate if three fold repetition is in play.
    - Next step is to implement UCI communication.
    
    - Stockfish said a classifier vs random game had 21 blunders by classifier and 19 by random.
    - Rome wasn't built in a day.

--- engine.py
+++ engine.py
@@ -1,21 +1,211 @@
 import random
 
 import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
 
 import board
 
 
+# Original chess network
+# More closely resembles an image classifier than SkipNet
+class ChessNet(nn.Module):
+
+    def __init__(self):
+        super(ChessNet, self).__init__()
+        # 1 input channel, 64 output channels, 5x5 kernel
+        # First layer, pools to 4x4
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=5, padding=2)
+        self.pool1 = nn.MaxPool2d(2, 2)
+
+        # Second layer, pools to 2x2
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.pool2 = nn.MaxPool2d(2, 2)
+
+        # Third layer, pools to 1x1
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.pool3 = nn.MaxPool2d(2, 2)
+
+        # Linear operations
+        self.fc1 = nn.Linear(256, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 1)
+        # 3 outputs, 2 = White win, 1 = draw, 0 = black win
+
+    def forward(self, x):
+
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool3(F.relu(self.conv3(x)))
+
+        # view essentially resizes a tensor, -1 means it'll infer that dimension
+        x = x.view(-1, 256)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+class SkipNet(nn.Module):
+
+    def __init__(self):
+        super(SkipNet, self).__init__()
+        # 1 input channel, 64 output channels, 3x3 kernel, no padding.
+        # This leads to a 64x6x6 output
+        # Pools to 3x3
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3)
+        self.pool1 = nn.MaxPool2d(2, 2)
+
+        # Batch normalization to speed training.
+        self.norm = nn.BatchNorm2d(64)
+
+        # Skip layer definitions, first uses 8x8 kernel to reduce the 8x8
+        # board to a 1x1 with 128 channels.
+        # Second uses a 3x3 kernel to go from 64x3x3 to 128x1x1
+        self.skip1 = nn.Conv2d(1, 128, kernel_size=8)
+        self.skip2 = nn.Conv2d(64, 128, kernel_size=3)
+
+        # Second layer, convolution with kernel size 2x2 reduce 3x3 to 2x2.
+        # Then gets pooled to 1x1.
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=2)
+        self.pool2 = nn.MaxPool2d(2, 2)
+
+        # Final layer with a 1x1 kernel. This layer will be joined with the
+        # two skip layers later.
+        self.conv3 = nn.Conv2d(128, 128, kernel_size=1)
+
+        # Linear operations
+        self.fc1 = nn.Linear(128*3, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 3)
+
+    def forward(self, x):
+
+        # Generate skip layer one, then run through layer 1.
+        x1 = self.skip1(x)
+        x = self.conv1(x)
+        x = self.norm(self.pool1(x))
+
+        # Generate skip layer two then run through layer 2
+        x2 = self.skip2(x)
+        x = self.pool2(self.conv2(x))
+
+        # Layer 3
+        x = self.conv3(x)
+
+        # Initially I concatenated x with x1 and x2, but by accident I later
+        # concatted x1 and x2 with the sum. It worked better so I left it.
+        xcat = x + x1 + x2
+        xcat = torch.cat((xcat, x1, x2), 1)
+
+        # Resize to the right shape for linear.
+        xcat = xcat.view(-1, 128*3)
+
+        # Linear layers.
+        xcat = F.relu(self.fc1(xcat))
+        xcat = F.relu(self.fc2(xcat))
+        xcat = self.fc3(xcat)
+        return xcat
+
+
 class Engine():
 
-    def __init__(self, color, new_board):
+    def __init__(self, color, new_board, impl="net"):
         self.color = color
         self.board = new_board
 
-    # In the future this will handle everyting, evaluating, then searching
-    # and so on and so forth until it finds a good move.
-    # For now I'm just going to return a random move lol.
+        self.brain = SkipNet()
+        self.brain.load_state_dict(torch.load("SkipNet-49.pt", map_location="cpu"))
+
+        # Sets the network to evaluation mode.
+        # There's a batch normalization layer which runs differently in
+        # training and evaluation.
+        self.brain.eval()
+
+        # Keeping that random implemenation around for testing.
+        if impl == "random":
+            self.eval = self.random_move
+        else:
+            self.eval = self.evaluate_moves
+
+
     def find_move(self):
         moves = self.board.generate_moves(self.color)
 
-        move = random.choice(moves)
-        return move
+        return self.eval(moves)
+
+
+    # Evaluates and returns the best move for now.
+    # TODO refactor to return move evaluations, use find move to run search
+    def evaluate_moves(self, moves):
+        states = []
+
+        # Transformation object, converts to a tensor then normalizes.
+        normalize = transforms.Normalize(mean=[0.485], std=[0.229])
+        trans = transforms.Compose([transforms.ToTensor(), normalize])
+
+        # Goes through each moves, converts it to a long move, and then
+        # gets the board state for that long algebraic.
+        for m in moves:
+            long_move = self.board.short_algebraic_to_long_algebraic(m)
+
+            if "O-O" in long_move or "0-0" in long_move:
+                s = self.board.castle_algebraic_to_boardstate(long_move)
+
+            else:
+                s = self.board.long_algebraic_to_boardstate(long_move)
+
+            # Once we have the state, we run the same conversions on it that
+            # were run when the network was trained.
+            s = s.astype("uint8")
+            s = np.where(s == 0, 128, s)
+            s = s.reshape((8, 8, 1))
+            s = trans(s)
+            s.resize_((1, 1, 8, 8))
+            states.append(s)
+
+        states = torch.cat(states, 0)
+
+        # Runs the boards through the network, and then gets their label.
+        # Remember:
+        # 0 = draw, 1 = black win, 2 = white win.
+        outputs = self.brain(states)
+        _, label = torch.max(outputs.data, 1)
+
+        # Shifts the outputs to numpy ndarrays.
+        outs = outputs.data.numpy()
+        labels = label.numpy()
+
+        # The liklihood values for wins and draws.
+        win_index = 2 if self.color == board.Color.WHITE else 1
+        wins = outs[labels == win_index]
+        draws = outs[labels == 0]
+
+        if len(wins) > 0:
+            # Looks for the move evaluation with the highest liklihood of
+            # being a black win.
+            best = np.argmax(wins[..., win_index])
+
+            # This reduces the moves array to just the moves that correspond
+            # with the win evaluations.
+            b_moves = np.asarray(moves)[labels == win_index]
+            return b_moves[best]
+        elif len(draws) > 0:
+            best = np.argmax(draws[..., 0])
+            # This reduces the moves array to just the moves that correspond
+            # with the draw evaluations.
+            b_moves = np.asarray(moves)[labels == 0]
+            return b_moves[best]
+
+        # If for some reason every move is winning for the opposite side
+        # just return the first move.
+        # This is lazy.
+        # TODO evaluation for all opposite win moves.
+        return moves[0]
+
+
+    def random_move(self, moves):
+        return random.choice(moves)
+

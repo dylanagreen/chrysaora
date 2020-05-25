d51c67576f15e81d15a1e6a3f9d4d7564617e6b5
LegoBricker <dylan@mygreenfamily.us>
2020-05-25 14:00:16 -0700

    Change 2 to a min_states variable.

--- src/train.nim
+++ src/train.nim
@@ -64,18 +64,20 @@ proc update_training_parameters*(board: Board, eval: float, pv: string) =
     board.unmake_move()
 
 proc update_weights*() =
-  # Without two states you can't calculate a difference (cuz what would it be
-  # a difference between?
-  if evals.len < 2:
+  # Without two states you can't calculate a difference
+  # I made min_states a variable in case we want to discount the opening
+  # moves since that's typically an open book kind of thing.
+  var min_states = 2
+  if evals.len < min_states:
     logging.debug("Not enough states to compute temporal difference")
-    logging.debug("At least two states required")
+    logging.debug(&"At least {min_states} states required")
     evals = @[]
     grads = @[]
     return
 
   var running_diff = 0.0
   # Works backwards from the end, stops at 2  because that gives the first two.
-  for i in countdown(evals.len, 2):
+  for i in countdown(evals.len, min_states):
     let diff = evals[i-1] - evals[i-2]
 
     # Computes the eligability trace

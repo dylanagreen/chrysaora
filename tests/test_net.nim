17f76f085a6591181084848119328552d833c98d
dylanagreen <dylan@mygreenfamily.us>
2021-04-04 15:40:34 -0700

    Add tests for position tensors.

--- tests/test_net.nim
+++ tests/test_net.nim
@@ -8,13 +8,34 @@ include ../src/net
 # Everything about this file was tedious as hell to write.
 suite "board state to tensor":
 
+# Empty tensor for testing:
+#  let expected = [0.0, 0.0, 0.0, 0.0, 0.0, # Piece difference
+#               1.0, # Side to move (WHITE)
+#               0.0, 0.0, # Castling rights
+#               0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Knight Ranks
+#               0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Knight Files
+#               0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Bishop Ranks
+#               0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Bishop Files
+#               0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Rook Ranks
+#               0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Rook Files
+#               0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Queen Ranks
+#               0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 # Queen Files
+
   test "start pos":
     let board = new_board()
     let observed = board.prep_board_for_network()
-    let expected = [0, 0, 0, 0, 0 # Piece difference
-                    # 1, # Side to move (WHITE)
-                    # 1, 1, 1, 1 # Castling rights
-                    ].toTensor().astype(float32) # Black King
+    let expected = [0.0, 0.0, 0.0, 0.0, 0.0, # Piece difference
+                    1.0, # Side to move (WHITE)
+                    0.0, 0.0, # Castling rights
+                    -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, # Knight Ranks
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Knight Files
+                    -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, # Bishop Ranks
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Bishop Files
+                    -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, # Rook Ranks
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Rook Files
+                    -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, # Queen Ranks
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 # Queen Files
+                    ].toTensor().astype(float32)
 
     check(observed == expected)
 
@@ -23,20 +44,36 @@ suite "board state to tensor":
     let board = load_fen("4R3/4k1p1/4p1Bp/3bN3/8/5PK1/3r2PP/4n3 b - - 7 46")
     let observed = board.prep_board_for_network()
 
-    let expected = [0, 0, 0, 0, 0 # Piece difference
-                    # -1, # Side to move (BLACK)
-                    # 0, 0, 0, 0 # Castling rights
-                    ].toTensor().astype(float32) # Black king
+    let expected = [0.0, 0.0, 0.0, 0.0, 0.0, # Piece difference
+                    -1.0, # Side to move (BLACK)
+                    0.0, 0.0, # Castling rights
+                    0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, -0.5, # Knight Ranks
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Knight Files
+                    0.0, 0.0, 0.5, -0.5, 0.0, 0.0, 0.0, 0.0, # Bishop Ranks
+                    0.0, 0.0, 0.0, -0.5, 0.0, 0.0, 0.5, 0.0, # Bishop Files
+                    0.5, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5, 0.0, # Rook Ranks
+                    0.0, 0.0, 0.0, -0.5, 0.5, 0.0, 0.0, 0.0, # Rook Files
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Queen Ranks
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 # Queen Files
+                    ].toTensor().astype(float32)
 
     check(observed == expected)
 
   test "color swap start pos":
     let board = new_board()
     let observed = board.prep_board_for_network().color_swap_board()
-    let expected = [0, 0, 0, 0, 0 # Piece difference
-                    # -1, # Side to move (BLACK)
-                    # 1, 1, 1, 1 # Castling rights
-                    ].toTensor().astype(float32) # Black King
+    let expected = [0.0, 0.0, 0.0, 0.0, 0.0, # Piece difference
+                    -1.0, # Side to move (BLACK)
+                    0.0, 0.0, # Castling rights
+                    -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, # Knight Ranks
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Knight Files
+                    -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, # Bishop Ranks
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Bishop Files
+                    -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, # Rook Ranks
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Rook Files
+                    -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, # Queen Ranks
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 # Queen Files
+                    ].toTensor().astype(float32)
 
     check(observed == expected)
 
@@ -46,23 +83,61 @@ suite "board state to tensor":
     # A position from AllieStein v0.2 vs Xiphos 0.5.2, March 24, 2019
     let board = load_fen("2qbrr2/p4pk1/1p5p/2p1N3/P2pPP2/1P1P2P1/5R2/Q4KRr w - - 0 34")
     let observed = board.prep_board_for_network()
-    let expected = [0.0, 0.5, -0.5, -0.5, 0.0 # Piece difference
-                    # 1, # Side to move (WHITE)
-                    # 0, 0, 0, 0 # Castling rights
-                    ].toTensor().astype(float32) # Black king
+
+    let expected = [0.0, 0.5, -0.5, -0.5, 0.0, # Piece difference
+                    1.0, # Side to move (WHITE)
+                    0.0, 0.0, # Castling rights
+                    0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, # Knight Ranks
+                    0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, # Knight Files
+                    -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Bishop Ranks
+                    0.0, 0.0, 0.0, -0.5, 0.0, 0.0, 0.0, 0.0, # Bishop Files
+                    -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, # Rook Ranks
+                    0.0, 0.0, 0.0, 0.0, -0.5, 0.0, 0.5, -0.5, # Rook Files
+                    -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, # Queen Ranks
+                    1.0, 0.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0 # Queen Files
+                    ].toTensor().astype(float32)
 
     check(observed == expected)
 
   test "black to move, white 3 rooks (reversed previous)":
-    # Atest to make sure we not only generate promotions correctly, but that
+    # A test to make sure we not only generate promotions correctly, but that
     # we put them in the correct place, i.e. the white section.
     # A position from AllieStein v0.2 vs Xiphos 0.5.2, March 24, 2019
     let board = load_fen("2qbrr2/p4pk1/1p5p/2p1N3/P2pPP2/1P1P2P1/5R2/Q4KRr w - - 0 34")
     let observed = board.prep_board_for_network().color_swap_board()
-    let expected = [0.0, -0.5, 0.5, 0.5, 0.0 # Piece difference
-                    # -1, # Side to move (BLACK)
-                    # 0, 0, 0, 0 # Castling rights
-                    ].toTensor().astype(float32) # Black king
+    let expected = [0.0, -0.5, 0.5, 0.5, 0.0, # Piece difference
+                    -1.0, # Side to move (BLACK)
+                    0.0, 0.0, # Castling rights
+                    0.0, 0.0, 0.0, 0.0, -0.5, 0.0, 0.0, 0.0, # Knight Ranks
+                    0.0, 0.0, 0.0, 0.0, -0.5, 0.0, 0.0, 0.0, # Knight Files
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, # Bishop Ranks
+                    0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, # Bishop Files
+                    0.0, -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, # Rook Ranks
+                    0.0, 0.0, 0.0, 0.0, 0.5, 0.0, -0.5, 0.5, # Rook Files
+                    -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, # Queen Ranks
+                    -1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0 # Queen Files
+                    ].toTensor().astype(float32)
+
+    check(observed == expected)
+
+  test "white to move, unequal castling rights":
+    # Testing to make sure that we're putting the castling rights into the right place
+    # in the network tensor.
+    # Position from lichess puzzle, March 17, 2021
+    let board = load_fen("1r3Bk1/3P2pp/p3pq2/8/3PKp2/2PQ1P2/P6P/R6R w Kq - 0 1")
+    let observed = board.prep_board_for_network()
+    let expected = [0.125, 0.0, 0.5, 0.5, 0.0, # Piece difference
+                    1.0, # Side to move (WHITE)
+                    1.0, -1.0, # Castling rights
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Knight Ranks
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Knight Files
+                    0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # Bishop Ranks
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, # Bishop Files
+                    -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, # Rook Ranks
+                    0.5, -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, # Rook Files
+                    0.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0, 0.0, # Queen Ranks
+                    0.0, 0.0, 0.0, 1.0, 0.0, -1.0, 0.0, 0.0 # Queen Files
+                    ].toTensor().astype(float32)
 
     check(observed == expected)
 
